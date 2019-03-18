e216625700a4c1c12a05b24a944067a9a44d325f
Esegbona Kelvin <kevoesegbona@gmail.com>
2019-03-18 22:48:59 +0100

    feat(user):  implement new message end point [Delivers #164710756]

--- Server/Controllers/DB/DBMsgController.js
+++ Server/Controllers/DB/DBMsgController.js
@@ -7,6 +7,46 @@ import { pool } from '../../helper/db_query/queryMethod';
 const { toDBArray } = helper;
 
 class EpicMessage {
+  static async newMessage(req, res) {
+    let { parentMessageId } = req.body;
+    if (parentMessageId === undefined) parentMessageId = null;
+    try {
+      const {
+        subject, message, status, receiverEmail,
+      } = req.body;
+      const [receiver] = await CRUD.find('users', 'email', receiverEmail);
+      if (!receiver) return errorResponse(400, 'Receiver email does not exist', res);
+      const receiverId = receiver.id;
+      const senderId = req.decoded;
+      if (senderId === receiverId) return errorResponse(400, 'Unable to create email', res);
+      const createdOn = new Date();
+      const receiverDelete = false;
+      const msgObj = {
+        createdOn,
+        subject,
+        message,
+        receiverId,
+        senderId,
+        parentMessageId,
+        status,
+        receiverDelete,
+      };
+      const [newData] = await CRUD.insert('messages',
+        '(created_on, subject, message, receiver_id, sender_id, parent_message_id, status, receiver_del)',
+        toDBArray(msgObj));
+      const { id } = newData;
+      return res.status(200).send({
+        status: 'Successful',
+        data: {
+          id, createdOn, subject, message, parentMessageId, status,
+        },
+      });
+    } catch (err) {
+      return errorResponse(400, 'Unable to create email', res);
+    }
+  }
+
+
   static async specificMessage(req, res) {
     const messageId = req.params.id;
     const userId = req.decoded;
