6f8e08a7f1b413bad308e1b847dbcaf56d94c294
Esegbona Kelvin <kevoesegbona@gmail.com>
2019-03-19 12:48:35 +0100

    feat(group) ensures user can create group [Delivers #164685743]

--- Server/Controllers/groupController.js
+++ Server/Controllers/groupController.js
@@ -1,29 +1,28 @@
 import errorResponse from '../helper/errorResponse';
-import database from '../helper/crud';
+import CRUD from '../helper/db_query/crud_db';
 
 class EpicGroup {
-  static newGroup(req, res) {
+  static async newGroup(req, res) {
     const { name } = req.body;
-    const groupOwnerId = req.decoded;
-
-    const groupObj = {
-      name,
-      groupOwnerId,
-      users: [req.decoded],
-      role: 'none',
-    };
-    const newData = database.add('groups', groupObj);
-    let { id, role } = newData;
-    role = 'owner';
-    return res.status(200).send({
-      status: 'Successful',
-      data: {
-        id,
-        name,
-        role,
-      },
-    });
+    const ownerId = req.decoded;
+    try {
+      const itExist = await CRUD.find('groups', 'name', name);
+      if (!itExist) {
+        await CRUD.insert('groups', '(name, group_owner_id)', [name, ownerId]);
+        return res.status(200).send({
+          status: 'Successful',
+          data: {
+            name,
+            role: 'owner',
+          },
+        });
+      }
+    } catch (err) {
+      errorResponse(500, 'Something went wrong', res);
+    }
+    return errorResponse(409, 'Group name already exist', res);
   }
 }
+
 
 export default EpicGroup;
