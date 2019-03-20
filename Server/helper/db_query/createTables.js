7360cc159247a131a79e5799dca7b61f7c3fe268
Esegbona Kelvin <kevoesegbona@gmail.com>
2019-03-21 00:15:45 +0100

    Fix(unread): fix unread messages to remove irrelevant table [Fixes #164787847]

--- Server/helper/db_query/createTables.js
+++ Server/helper/db_query/createTables.js
@@ -7,7 +7,6 @@ const { toDBArray } = someFxn;
 
 const userObj = database.getStorage('users');
 const msgObj = database.getStorage('messages');
-const readObj = database.getStorage('read');
 
 
 const users = `CREATE TABLE IF NOT EXISTS
@@ -30,15 +29,10 @@ const messages = `CREATE TABLE IF NOT EXISTS
           parent_message_id integer,
           status text ,
           receiver_del BOOL ,
+          read_stat text,
           groupid integer REFERENCES groups(id)
         );`;
 
-const read = `CREATE TABLE IF NOT EXISTS
-        read(
-          id serial PRIMARY KEY,
-          user_id integer NOT NULL,
-          message_id integer NOT NULL
-        );`;
 
 const groups = `CREATE TABLE IF NOT EXISTS
         groups(
@@ -55,7 +49,7 @@ const groupJoin = `CREATE TABLE IF NOT EXISTS
           FOREIGN KEY (member) REFERENCES users(id) ON DELETE CASCADE
         );`;
 
-const create = `${users}${groups}${groupJoin}${messages}${read}`;
+const create = `${users}${groups}${groupJoin}${messages}`;
 const populateDB = async () => {
   await pool.query(create);
   try {
@@ -64,24 +58,16 @@ const populateDB = async () => {
         toDBArray(obj));
     });
     msgObj.forEach((obj) => {
-      CRUD.insert('messages', '(created_on, subject, message, receiver_id, sender_id, parent_message_id, status, receiver_del)',
+      CRUD.insert('messages', '(created_on, subject, message, receiver_id, sender_id, parent_message_id, status, receiver_del, read_stat)',
         toDBArray(obj));
-    });
-    readObj.forEach((obj) => {
-      CRUD.insert('read', '(user_id, message_id)', toDBArray(obj));
     });
 
     CRUD.insert('groups', '(name, admin)', ['great', 1]);
     CRUD.insert('groups', '(name, admin)', ['it has been God all the way', 2]);
     CRUD.insert('joint', '(group_id, member)', [1, 1]);
-    // CRUD.insert('joint', '(group_id, member)', [1, 2]);
     CRUD.insert('joint', '(group_id, member)', [2, 1]);
   } catch (err) { console.log(err); }
 };
 
 populateDB();
 
-// pool.on('remove', () => {
-//   console.log('client removed');
-//   process.exit(0);
-// });
