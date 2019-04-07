c03af5f900329ff0260e718cd2b47812b10fc564
Esegbona Kelvin <kevoesegbona@gmail.com>
2019-04-07 11:37:58 +0100

    feat(user) : restructure sign-uppage [Delivers #165122535]

--- UI/JS/helpers.js
+++ UI/JS/helpers.js
@@ -1,20 +1,17 @@
 const token = (!localStorage.token) ? null : localStorage.token;
 
+
 const fetchCall = async (url, method, body = undefined) => {
   const object = {
     method,
     headers: new Headers({
       'Content-Type': 'application/json',
-      token,
     }),
     body: JSON.stringify(body),
   };
   const response = await fetch(url, object);
+  const statusCode = response.status;
   const responseObj = await response.json();
-  const statusCode = responseObj.status;
-  if (statusCode === 200) { return responseObj; }
-  return false;
+  console.log(responseObj);
+  return { responseObj, statusCode };
 };
-
-
-export default fetchCall;
